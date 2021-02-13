654e40ff2d2afd60457f57e1b67caa74daa570e0
Callum Irving <callum.irving04@gmail.com>
2021-02-13 14:55:50 -0500

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+alias ls="exa -al --color=always --group-directories-first"
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
+neofetch
+starship init fish | source
---
+++ .vimrc
@@ -0,0 +1,11 @@
+set nu
+set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
+set nocompatible
+filetype plugin on
+syntax on
+
+:nnoremap <Space> @q
+
+call plug#begin()
+Plug 'vimwiki/vimwiki'
+call plug#end()
